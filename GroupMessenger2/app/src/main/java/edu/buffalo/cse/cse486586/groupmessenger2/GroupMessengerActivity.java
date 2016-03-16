9c827e24a3e433358145732dd27324d425c864dd
Prasad Shivanna <prasadsjcecs@gmail.com>
2016-03-16 12:02:07 -0400

    working with 2A status

--- GroupMessenger2/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/GroupMessengerActivity.java
+++ GroupMessenger2/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/GroupMessengerActivity.java
@@ -1,10 +1,27 @@
 package edu.buffalo.cse.cse486586.groupmessenger2;
 
 import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  * GroupMessengerActivity is the main Activity for the assignment.
@@ -14,30 +31,154 @@ import android.widget.TextView;
  */
 public class GroupMessengerActivity extends Activity {
 
+    static final String TAG = GroupMessengerActivity.class.getSimpleName();
+    private static final String KEY_FIELD = "key";
+    private static final String VALUE_FIELD = "value";
+    static final int[] REMOTE_PORTS = {11108, 11112};//, 11116, 11120, 11124};
+    static final int SERVER_PORT = 10000;
+    static int messageCounter = 0;
+    private ContentResolver contentResolver;
+    private final Uri uri = buildUri("content", "edu.buffalo.cse.cse486586.groupmessenger2.provider");
+    private static ContentValues contentValues = new ContentValues();
+
+    /**
+     * buildUri() demonstrates how to build a URI for a ContentProvider.
+     *
+     * @param scheme
+     * @param authority
+     * @return the URI
+     */
+    private Uri buildUri(String scheme, String authority) {
+        Uri.Builder uriBuilder = new Uri.Builder();
+        uriBuilder.authority(authority);
+        uriBuilder.scheme(scheme);
+        return uriBuilder.build();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_messenger);
 
-        /*
-         * TODO: Use the TextView to display your messages. Though there is no grading component
-         * on how you display the messages, if you implement it, it'll make your debugging easier.
-         */
         TextView tv = (TextView) findViewById(R.id.textView1);
         tv.setMovementMethod(new ScrollingMovementMethod());
-        
+
         /*
          * Registers OnPTestClickListener for "button1" in the layout, which is the "PTest" button.
          * OnPTestClickListener demonstrates how to access a ContentProvider.
          */
         findViewById(R.id.button1).setOnClickListener(
                 new OnPTestClickListener(tv, getContentResolver()));
-        
-        /*
-         * TODO: You need to register and implement an OnClickListener for the "Send" button.
-         * In your implementation you need to get the message from the input box (EditText)
-         * and send it to other AVDs.
-         */
+
+        contentResolver = getContentResolver();
+        try {
+            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+            new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
+        } catch (IOException e) {
+            Log.e(TAG, "Can't create a ServerSocket");
+            return;
+        }
+        Button sendButton = (Button) findViewById(R.id.button4);
+        sendButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = (EditText) findViewById(R.id.editText1);
+                String msg = editText.getText().toString() + "\n";
+                editText.setText("");
+                new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg);
+            }
+        });
+    }
+
+
+    /***
+     * ServerTask is an AsyncTask that should handle incoming messages. It is created by
+     * ServerTask.executeOnExecutor() call in SimpleMessengerActivity.
+     *
+     * Please make sure you understand how AsyncTask works by reading
+     * http://developer.android.com/reference/android/os/AsyncTask.html
+     *
+     * @author stevko
+     *
+     */
+    private class ServerTask extends AsyncTask<ServerSocket, String, Void> {
+
+        @Override
+        protected synchronized Void doInBackground(ServerSocket... sockets) {
+            ServerSocket serverSocket = sockets[0];
+            while(true){
+                try {
+                    Socket socket =  serverSocket.accept();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    String[] msgRecieved;
+                    String messages;
+                    messages = reader.readLine();
+                    if(messages != null && messages.trim().length() > 0){
+                        msgRecieved= messages.split("\\\n+");
+                        if(msgRecieved != null){
+                            Log.i(TAG,"recM: " + msgRecieved);
+                            contentValues.put(KEY_FIELD,Integer.toString(messageCounter));
+                            contentValues.put(VALUE_FIELD, msgRecieved[0].trim());
+                            Log.i(TAG,"rec: " + contentValues);
+                            contentResolver.insert(uri, contentValues);
+                            messageCounter++;
+                            publishProgress(msgRecieved);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        protected void onProgressUpdate(String...strings) {
+            /*
+             * The following code displays what is received in doInBackground().
+             */
+            String strReceived = strings[0].trim();
+            TextView textView = (TextView) findViewById(R.id.textView1);
+            textView.append(strReceived + "\n");
+
+            /*
+             * The following code creates a file in the AVD's internal storage and stores a file.
+             *
+             * For more information on file I/O on Android, please take a look at
+             * http://developer.android.com/training/basics/data-storage/files.html
+             */
+            return;
+        }
+    }
+
+    /***
+     * ClientTask is an AsyncTask that should send a string over the network.
+     * It is created by ClientTask.executeOnExecutor() call whenever OnKeyListener.onKey() detects
+     * an enter key press event.
+     *
+     * @author stevko
+     *
+     */
+    private class ClientTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... msgs) {
+            for(int port: REMOTE_PORTS){
+                try {
+                    Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}), port);
+                    PrintWriter writer = new PrintWriter(socket.getOutputStream(),true);
+                    String msgToSend = msgs[0];
+                    Log.i(TAG, "Message to send: " + msgToSend);
+                    if(socket.isConnected()){
+                        writer.println(msgToSend);
+                    }
+                    socket.close();
+                } catch (UnknownHostException e) {
+                    Log.e(TAG, "ClientTask UnknownHostException");
+                } catch (IOException e) {
+                    Log.e(TAG, "ClientTask socket IOException");
+                }
+            }
+            return null;
+        }
     }
 
     @Override
--- GroupMessenger2/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/GroupMessengerProvider.java
+++ GroupMessenger2/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/GroupMessengerProvider.java
@@ -1,30 +1,107 @@
-package edu.buffalo.cse.cse486586.groupmessenger2;
+package edu.buffalo.cse.cse486586.groupmessenger1;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.text.TextUtils;
 import android.util.Log;
+
+import java.util.HashMap;
 
 /**
  * GroupMessengerProvider is a key-value table. Once again, please note that we do not implement
  * full support for SQL as a usual ContentProvider does. We re-purpose ContentProvider's interface
  * to use it as a key-value table.
- * 
+ *
  * Please read:
- * 
+ *
  * http://developer.android.com/guide/topics/providers/content-providers.html
  * http://developer.android.com/reference/android/content/ContentProvider.html
- * 
+ *
  * before you start to get yourself familiarized with ContentProvider.
- * 
+ *
  * There are two methods you need to implement---insert() and query(). Others are optional and
  * will not be tested.
- * 
+ *
  * @author stevko
  *
  */
 public class GroupMessengerProvider extends ContentProvider {
+
+    /*
+     *  SQL lite specific declarations
+     *  TO create DB and tables
+     */
+    private SQLiteDatabase db;
+    private static final String DATABASE_NAME = "DSSpring2016";
+    private static final String KEY_VALUE_TABLE = "KeyValueTable";
+    private static final String KEY_COLUMN_NAME = "key";
+    private static final String VALUE_COLUMN_NAME = "value";
+    private static final int DATABASE_VERSION = 1;
+    private static final String CREATE_TABLE_SQL_QUERY = "CREATE TABLE " + KEY_VALUE_TABLE +
+            "( "+ KEY_COLUMN_NAME +" TEXT PRIMARY KEY NOT NULL, " + VALUE_COLUMN_NAME +" TEXT NOT NULL);";
+
+    static final Uri CONTENT_URI = Uri.parse("edu.buffalo.cse.cse486586.groupmessenger2.GroupMessengerProvider");
+
+    private static class DatabaseHelper extends SQLiteOpenHelper{
+
+        DatabaseHelper(Context context){
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+        /**
+         * Called when the database is created for the first time. This is where the
+         * creation of tables and the initial population of the tables should happen.
+         *
+         * @param db The database.
+         */
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(CREATE_TABLE_SQL_QUERY);
+        }
+
+        /**
+         * Called when the database needs to be upgraded. The implementation
+         * should use this method to drop tables, add tables, or do anything else it
+         * needs to upgrade to the new schema version.
+         * <p>
+         * <p>
+         * The SQLite ALTER TABLE documentation can be found
+         * <a href="http://sqlite.org/lang_altertable.html">here</a>. If you add new columns
+         * you can use ALTER TABLE to insert them into a live table. If you rename or remove columns
+         * you can use ALTER TABLE to rename the old table, then create the new table and then
+         * populate the new table with the contents of the old table.
+         * </p><p>
+         * This method executes within a transaction.  If an exception is thrown, all changes
+         * will automatically be rolled back.
+         * </p>
+         *
+         * @param db         The database.
+         * @param oldVersion The old database version.
+         * @param newVersion The new database version.
+         */
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + KEY_VALUE_TABLE);
+            onCreate(db);
+        }
+    }
+
+    @Override
+    public boolean onCreate() {
+        // If you need to perform any one-time initialization task, please do it here.
+
+        DatabaseHelper databaseHelper = new DatabaseHelper(getContext());
+        db = databaseHelper.getWritableDatabase();
+        return !(db == null);
+    }
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
@@ -44,21 +121,24 @@ public class GroupMessengerProvider extends ContentProvider {
          * TODO: You need to implement this method. Note that values will have two columns (a key
          * column and a value column) and one row that contains the actual (key, value) pair to be
          * inserted.
-         * 
+         *
          * For actual storage, you can use any option. If you know how to use SQL, then you can use
          * SQLite. But this is not a requirement. You can use other storage options, such as the
          * internal storage option that we used in PA1. If you want to use that option, please
          * take a look at the code for PA1.
          */
+
         Log.v("insert", values.toString());
-        return uri;
+        long rowID = db.replace(KEY_VALUE_TABLE, "", values);
+        if(rowID > 0)
+        {
+            uri = ContentUris.withAppendedId(CONTENT_URI, rowID);
+            getContext().getContentResolver().notifyChange(uri, null);
+            return uri;
+        }
+        throw new SQLException("Failed to add a record into " + uri);
     }
 
-    @Override
-    public boolean onCreate() {
-        // If you need to perform any one-time initialization task, please do it here.
-        return false;
-    }
 
     @Override
     public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
@@ -81,6 +161,13 @@ public class GroupMessengerProvider extends ContentProvider {
          * http://developer.android.com/reference/android/database/MatrixCursor.html
          */
         Log.v("query", selection);
-        return null;
+
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        queryBuilder.setTables(KEY_VALUE_TABLE);
+        queryBuilder.setProjectionMap(new HashMap<String, String>());
+        queryBuilder.appendWhere(KEY_COLUMN_NAME + " = '" + selection +"'");
+        Cursor cursor = queryBuilder.query(db, projection, null, selectionArgs, null, null,sortOrder);
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
     }
 }
