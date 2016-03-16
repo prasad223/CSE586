e1693637e7fe31ee094f908588798409c6f94401
Prasad Shivanna <prasadsjcecs@gmail.com>
2016-03-16 12:41:49 -0400

    able to send and recieve messages

--- GroupMessenger2/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/GroupMessengerActivity.java
+++ GroupMessenger2/app/src/main/java/edu/buffalo/cse/cse486586/groupmessenger2/GroupMessengerActivity.java
@@ -3,9 +3,11 @@ package edu.buffalo.cse.cse486586.groupmessenger2;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.ContentValues;
+import android.content.Context;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.Menu;
@@ -17,11 +19,17 @@ import android.widget.TextView;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
+import java.io.Serializable;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * GroupMessengerActivity is the main Activity for the assignment.
@@ -36,10 +44,92 @@ public class GroupMessengerActivity extends Activity {
     private static final String VALUE_FIELD = "value";
     static final int[] REMOTE_PORTS = {11108, 11112};//, 11116, 11120, 11124};
     static final int SERVER_PORT = 10000;
-    static int messageCounter = 0;
     private ContentResolver contentResolver;
     private final Uri uri = buildUri("content", "edu.buffalo.cse.cse486586.groupmessenger2.provider");
     private static ContentValues contentValues = new ContentValues();
+
+    static int myPort;
+    static AtomicInteger messageCounter = new AtomicInteger(0);
+    static AtomicInteger sequenceNumber = new AtomicInteger(0);
+    static PriorityBlockingQueue<Message> priorityBlockingQueue = new PriorityBlockingQueue<Message>();
+
+
+    static class Message implements Serializable,Comparable{
+
+        private static final long serialVersionUID = 237569878453498743L;
+        String message;
+        int senderId;
+        boolean isDeliverable;
+        double sequenceNumber;
+        int[] proposals;
+
+        Message(String message,int senderId){
+            this.message = message;
+            this.senderId = senderId;
+            this.isDeliverable = false;
+            this.sequenceNumber = Double.POSITIVE_INFINITY;
+            this.proposals = new int[REMOTE_PORTS.length];
+            for(int i=0;i<REMOTE_PORTS.length;i++){
+                proposals[i] = Integer.MAX_VALUE;
+            }
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof Message)) return false;
+
+            Message message1 = (Message) o;
+
+            if (senderId != message1.senderId) return false;
+            return message.equals(message1.message);
+
+        }
+
+        public void computeSequence(){
+            int maxIndex =0;
+            int maxProposal =0;
+            for(int i =0; i<proposals.length;i++){
+                if(proposals[i] >= maxProposal){
+                    maxProposal = proposals[i];
+                    maxIndex = Math.max(i+1,maxIndex);
+                }
+            }
+            sequenceNumber = (double)maxProposal + (maxIndex * 0.1);
+            if(senderId == myPort){
+                isDeliverable = maxProposal != Integer.MAX_VALUE;
+            }
+        }
+
+        @Override
+        public String toString(){
+            StringBuffer stringBuffer = new StringBuffer();
+            stringBuffer.append(this.senderId + ",");
+            stringBuffer.append(this.message + ",");
+            stringBuffer.append(this.isDeliverable+",");
+            stringBuffer.append(String.valueOf(sequenceNumber)+",");
+            stringBuffer.append(Arrays.toString(proposals));
+            return stringBuffer.toString();
+        }
+
+        @Override
+        public int compareTo(Object o) {
+            if(o == null || !(o instanceof Message)){
+                return -1;
+            }
+            return Double.compare(this.sequenceNumber,((Message)o).sequenceNumber);
+        }
+    }
+
+    static class WrapperMsg{
+        Message message;
+        int port;
+
+        WrapperMsg(Message message,int port){
+            this.message = message;
+            this.port = port;
+        }
+    }
 
     /**
      * buildUri() demonstrates how to build a URI for a ContentProvider.
@@ -71,6 +161,11 @@ public class GroupMessengerActivity extends Activity {
                 new OnPTestClickListener(tv, getContentResolver()));
 
         contentResolver = getContentResolver();
+        TelephonyManager tel = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        String portStr = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
+        myPort = Integer.parseInt(portStr)*2;
+        Log.i(TAG, " myport: " + myPort);
+
         try {
             ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
             new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
@@ -85,7 +180,9 @@ public class GroupMessengerActivity extends Activity {
                 EditText editText = (EditText) findViewById(R.id.editText1);
                 String msg = editText.getText().toString() + "\n";
                 editText.setText("");
-                new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, msg);
+                for(int port: REMOTE_PORTS){
+                    new ClientTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR,new WrapperMsg(new Message(msg,myPort),port));
+                }
             }
         });
     }
@@ -109,23 +206,14 @@ public class GroupMessengerActivity extends Activity {
             while(true){
                 try {
                     Socket socket =  serverSocket.accept();
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                    String[] msgRecieved;
-                    String messages;
-                    messages = reader.readLine();
-                    if(messages != null && messages.trim().length() > 0){
-                        msgRecieved= messages.split("\\\n+");
-                        if(msgRecieved != null){
-                            Log.i(TAG,"recM: " + msgRecieved);
-                            contentValues.put(KEY_FIELD,Integer.toString(messageCounter));
-                            contentValues.put(VALUE_FIELD, msgRecieved[0].trim());
-                            Log.i(TAG,"rec: " + contentValues);
-                            contentResolver.insert(uri, contentValues);
-                            messageCounter++;
-                            publishProgress(msgRecieved);
-                        }
-                    }
-                } catch (IOException e) {
+                    ObjectInputStream input  = new ObjectInputStream(socket.getInputStream());
+                    Object msg = input.readObject();
+                    Log.i(TAG,"msfg rec: " + msg);
+                    contentValues.put(KEY_FIELD,String.valueOf(messageCounter.getAndIncrement()));
+                    contentValues.put(VALUE_FIELD, ((Message)msg).toString());
+                    contentResolver.insert(uri, contentValues);
+                    publishProgress(msg.toString());
+                } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
@@ -157,26 +245,27 @@ public class GroupMessengerActivity extends Activity {
      * @author stevko
      *
      */
-    private class ClientTask extends AsyncTask<String, Void, Void> {
+    private class ClientTask extends AsyncTask<WrapperMsg, Void, Void> {
 
         @Override
-        protected Void doInBackground(String... msgs) {
-            for(int port: REMOTE_PORTS){
+        protected synchronized Void doInBackground(WrapperMsg... msgs) {
                 try {
+                    Message msg = msgs[0].message;
+                    int port = msgs[0].port;
+                    Log.i(TAG, "Message to send: " + msg + " onport: " + port);
                     Socket socket = new Socket(InetAddress.getByAddress(new byte[]{10, 0, 2, 2}), port);
-                    PrintWriter writer = new PrintWriter(socket.getOutputStream(),true);
-                    String msgToSend = msgs[0];
-                    Log.i(TAG, "Message to send: " + msgToSend);
+                    ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
                     if(socket.isConnected()){
-                        writer.println(msgToSend);
+                        out.writeObject(msg);
                     }
+                    out.flush();
+                    out.close();
                     socket.close();
                 } catch (UnknownHostException e) {
                     Log.e(TAG, "ClientTask UnknownHostException");
                 } catch (IOException e) {
                     Log.e(TAG, "ClientTask socket IOException");
                 }
-            }
             return null;
         }
     }
