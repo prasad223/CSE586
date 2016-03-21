8416bd3fe350521881881d060fdc0b4c2b116f28
Prasad Shivanna <prasadsjcecs@gmail.com>
2016-03-20 22:39:49 -0400

    PA2B submitted successfully,PA3 begins

---
+++ SimpleDht/app/src/main/java/edu/buffalo/cse/cse486586/simpledht/SimpleDhtActivity.java
@@ -0,0 +1,75 @@
+package edu.buffalo.cse.cse486586.simpledht;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.app.Activity;
+import android.telephony.TelephonyManager;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.HashMap;
+
+public class SimpleDhtActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_dht_main);
+        
+        TextView tv = (TextView) findViewById(R.id.textView1);
+        tv.setMovementMethod(new ScrollingMovementMethod());
+        findViewById(R.id.button3).setOnClickListener(
+                new OnTestClickListener(tv, getContentResolver()));
+
+
+        TelephonyManager tel = (TelephonyManager) this.getSystemService(Context.TELEPHONY_SERVICE);
+        String portStr = tel.getLine1Number().substring(tel.getLine1Number().length() - 4);
+        myPort = Integer.parseInt(portStr)*2;
+        Log.i(TAG, " myport: " + myPort);
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(SERVER_PORT);
+            new ServerTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, serverSocket);
+        } catch (IOException e) {
+            Log.e(TAG, "Can't create a ServerSocket: ", e );
+            return;
+        }
+        Button sendButton = (Button) findViewById(R.id.button4);
+        sendButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editText = (EditText) findViewById(R.id.editText1);
+                String msg = editText.getText().toString();// + "\n";
+                editText.setText("");
+                Message message = new Message(msg, myPort, messageIdCounter.incrementAndGet());
+                try {
+                    failureHandleLock.lock();
+                    messageListMap.put(message.messageId, new HashMap<Integer, Integer>());
+                    messageMap.put(message.messageId, message);
+                    Log.i(TAG, "Generated New Message: " + message);
+                } finally {
+                    failureHandleLock.unlock();
+                }
+                new ClientTask().executeOnExecutor(AsyncTask.SERIAL_EXECUTOR, message);
+                startDetector();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_simple_dht_main, menu);
+        return true;
+    }
+
+}
